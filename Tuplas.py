4069822e723da067d5fb5cb9ba2bf92e1144b140
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-08-24 19:39:51 -0300

    testes

---
+++ Tuplas.py
@@ -0,0 +1,11 @@
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+#   tuplas são representadas entre parenteses ('Hambúrguer', 'Suco', 'Pizza',
+#  'Pudim') porém o python identifica que é uma tupla mesmo sem parenteses
+print(lanche)
+
+print(lanche[1])  # mostra o valor na posição 1
+
+print(lanche[:2])  # vai do inicio até o 1, pois ele ignora o último
+
+print(lanche[1:3])  # começa na posição 1 e vai até 2, ignorando o 3
+

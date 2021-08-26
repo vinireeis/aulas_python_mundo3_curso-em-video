b4bb1b0705d68803eecd44680eca71873fbeb0ab
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-08-26 18:44:02 -0300

    exer73 ok

---
+++ exer73-OrdenandoeFiltrandoTuplas.py
@@ -0,0 +1,15 @@
+br = ('Atlético-MG', 'Palmeiras', 'Fortaleza', 'Bragantino', 'Flamengo',
+      'Corinthians', 'Atlético-GO', 'Ceará', 'Athletico-PR', 'Internacional',
+      'Santos', 'São Paulo', 'Juventude', 'Cuiabá', 'Bahia', 'Fluminense',
+      'Grêmio', 'Sport', 'América-MG', 'Chapecoense')
+
+print(br)
+print('-' * 60)
+print(br[0:5])
+print('-' * 60)
+print(br[16:20])
+print('-' * 60)
+print(sorted(br))
+print('-' * 60)
+print(' O time da Chapecoense está na ' + str(br.index('Chapecoense') + 1) +
+      'ª posição')

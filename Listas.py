522422d03ed28a48a2f87671f2269b30c84276e5
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-08-29 20:28:15 -0300

    aula listas

---
+++ Listas.py
@@ -0,0 +1,37 @@
+lista = [2, 5, 9, 1]
+print(lista)
+lista[2] = 10  # alterando um elemento da lista
+print(lista)
+lista.append(3)  # adicionando um elemento no fim da lista
+print(lista)
+lista.sort(reverse=True)  # ordenação decrescente
+print(lista)
+lista.sort()
+print(lista)  # ordenação crescente
+lista.insert(2, 0)
+# inserindo um valor num index específico (2 é o index, 0 é o valor)
+print(lista)
+lista.pop(2)  # removendo um elemento pelo index
+print(lista)
+lista.remove(5)  # remove o primeiro '5' da lista
+print(lista)
+print(len(lista))  # verificar o tamanho da lista
+print('-' * 60)  # criando cópia e ligação entre lista
+a = [2, 3, 7, 1]
+b = a  # cria uma ligação entre as listas, fazendo com que ao alterar uma>
+#  ambas sejam alteradas
+b[2] = 9  # alterando o elemento da posição 2
+print('Listas interligadas')
+print(f'lista A: {a}, \nlista B:{b} ')
+''' agora se fizer da seguinte maneira
+C = [2, 3, 7, 1]
+D = C[:]
+Você estará fazendo uma cópia da lista, sem ligação, do qual ao alterar um
+elemento da lista B não afetará nenhum elemento da lista A
+'''
+print('-' * 60)
+print('Cópia de Lista')
+c = [2, 3, 7, 1]
+d = c[:]
+d[1] = 15
+print(f'lista C: {c}, \nlista D: {d} ')

a08744ae9ab00e8780038be924e85612f2fb4f11
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-09-02 00:42:19 -0300

    att exer 79

--- exer79-OrdenarVerificarLista.py
+++ exer79-OrdenarVerificarLista.py
@@ -1,10 +1,10 @@
 lista = []
-resp = 'S'
-while resp == 'S':
+while True:
     valor = int(input('Digite um valor: '))
     if valor not in lista:
         lista.append(valor)
-        print("O valor não foi adicionado pois já existe\n")
+    else:
+        print("O valor não foi adicionado pois já existe")
     resp = input('Deseja continuar? [S/N]: ').upper()
     if resp == 'N':
         break

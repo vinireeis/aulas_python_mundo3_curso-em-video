cfb51a39f0240857469473a1f21970d3fb4b6076
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-10-03 01:28:01 -0300

    funcoes

--- exer95+funcoes.py
+++ exer95+funcoes.py
@@ -9,8 +9,21 @@ def escreva(msg):
     print('~' * len(msg))
 
 
+def contagem(inicial, final, pulo):
+    for x in range(1, 10+1):
+        print(x, end=' ')
+    print()
+    for x in range(10, 0, -2):
+        print(x, end=' ')
+    print()
+    for x in range(inicial, final, pulo):
+        print(x, end=' ')
+
+
 if __name__ == '__main__':
     x = calc_area(4.5, 8)
     print(x)
 
     escreva('Amo a larissinha')
+
+    contagem(7, 0, -2)
